dc1bc419b0f784f0b60ae4cc3cdd96a9b5f94a7f
amandabhernandez <amandabhernandez@gmail.com>
2022-02-21 19:22:03 -0500

    change to dashboard layout

--- airthings_explorer_app/server.R
+++ airthings_explorer_app/server.R
@@ -7,126 +7,278 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(lubridate)
-
-# load and clean data
-
-air_dat <- read.csv("../data/2960014368-latest.csv", sep = ";")
-
-
-air_dat_cleaned <- air_dat %>% 
-    clean_names() %>%
-    separate(recorded, sep = "T", into = c("date", "time"), remove = FALSE) %>% 
-    mutate(date = ymd(date),
-           time = hms(time),
-           Time = ifelse(minute(time) >= 10, paste0(hour(time), ":", minute(time)),
-                         paste0(hour(time), ":0", minute(time))),
-           recorded = ymd_hms(recorded))
-
-
-air_dat_long <- air_dat_cleaned %>% 
-    pivot_longer(names_to = "metric",
-                 values_to = "Result",
-                 -c("recorded", "date", "time", "Time"),
-                 values_drop_na = TRUE) %>% 
-    mutate(metric = case_when(metric == "co2_ppm" ~ "CO2 (ppm)",
-                              metric == "pm1_mg_m3" ~ "PM1 (mg/m3)",
-                              metric == "pm2_5_mg_m3" ~ "PM2.5 (mg/m3)",
-                              metric == "pressure_h_pa" ~ "Pressure (mbar)",
-                              metric == "radon_short_term_avg_p_ci_l" ~ "Radon (pCi/L)",
-                              metric == "temp_f" ~ "Temperature (F)",
-                              metric == "voc_ppb" ~ "VOC (ppb)",
-                              metric == "humidity" ~ "Humidity (%)"))
-
-
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
     
-    output$date_range <- renderUI({
-        dateRangeInput("date_range", "Date Range:", 
-                       start = min(air_dat_long$date),
-                       end = max(air_dat_long$date),
-                       min = min(air_dat_long$date),
-                       max = max(air_dat_long$date))
-    })
+    # output$date_range <- renderUI({
+    #     dateRangeInput("date_range", "Date Range:", 
+    #                    start = min(air_dat_long$date),
+    #                    end = max(air_dat_long$date),
+    #                    min = min(air_dat_long$date),
+    #                    max = max(air_dat_long$date))
+    # })
     
-    output$metric <- renderUI({
-        pickerInput("metric", "Select Metric", 
-                       choices = as.list(unique(air_dat_long$metric)),
-                       options = list(`actions-box` = TRUE,
-                                      title = "Select metric(s)"),
-                       multiple = TRUE
-                       )
-    })
+    # output$metric <- renderUI({
+    #     pickerInput("metric", "Select Metric", 
+    #                 choices = as.list(unique(air_dat_long$metric)),
+    #                 options = list(`actions-box` = TRUE,
+    #                                title = "Select metric(s)"),
+    #                 multiple = TRUE
+    #     )
+    # })
     
     #filter data based on user input
-    dat <- reactive(
-        air_dat_long %>% 
-            filter(date >= min(input$date_range) &
-                       date  <= max(input$date_range)) %>% 
-            filter(metric %in% c(input$metric))
+    dat <- eventReactive(input$date_select, {
+    #reactive({
+        req(input$date_select)
+        if(input$date_select == "Date Range"){
+            air_dat_long %>% 
+                filter(date >= min(input$date_range) &
+                           date  <= max(input$date_range))  
+        }
         
-    )
+        else if(input$date_select == "12hr"){
+            air_dat_long %>%
+                filter(recorded >= now()-hours(12))
+        }
+        
+        else if(input$date_select == "24hr"){
+            air_dat_long %>%
+                filter(recorded >= now() - hours(24))
+        }
+        else if(input$date_select == "36hr"){
+            air_dat_long %>%
+                filter(recorded >= now() - hours(36))
+        }
+        else if(input$date_select == "48hr"){
+            air_dat_long %>%
+                filter(recorded >= now() - hours(48))
+        }
+        else if(input$date_select == "1wk"){
+            air_dat_long %>%
+                filter(recorded >= now() - weeks(1))
+        }
+        else if(input$date_select == "1mo"){
+            air_dat_long %>%
+                filter(recorded >= now() - months(1))
+        }
+        else{
+            air_dat_long
+        }
+        # else{
+        #     air_dat_long
+        # }
+        
+        #dat_date
+        #print(nrow(dat_date))
+        #print(input$date_select)
+        
+        
+    #})
+        })
+    
+    # icons from https://fontawesome.com/v4/icons/
+    output$temp_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$temp, "°F"),
+            title = "Temperature",
+            icon = icon("thermometer-half"),
+            color = ifelse(current_dat$temp > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                        "Temperature (F)")], "yellow",
+                           ifelse(current_dat$temp < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "Temperature (F)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$temp > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "Temperature (F)")], "it's warmer than usual",
+                              ifelse(current_dat$temp < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "Temperature (F)")],
+                                     "it's colder than usual", ""))
+        )
+    })
+    
+    output$humidity_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$humidity, "%"),
+            title = "Humidity",
+            icon = icon("tint"), 
+            color = ifelse(current_dat$humidity > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "Humidity (%)")], "yellow",
+                           ifelse(current_dat$humidity < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "Humidity (%)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$humidity > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "Humidity (%)")], "it's more humid than usual",
+                              ifelse(current_dat$humidity < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "Humidity (%)")],
+                                     "it's less humid than usual", ""))
+        )
+    })
+    
+    output$pressure_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$pressure, " hPa"),
+            title = "Pressure",
+            icon = icon("tachometer"), 
+            color = ifelse(current_dat$pressure > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "Pressure (mbar)")], "yellow",
+                           ifelse(current_dat$pressure < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "Pressure (mbar)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$pressure > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "Pressure (mbar)")], "the pressure is higher than usual",
+                              ifelse(current_dat$pressure < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "Pressure (mbar)")],
+                                     "the pressure is lower than usual", ""))
+        )
+    })
+    output$radon_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$radonShortTermAvg, " pCi/L"),
+            title = "Radon",
+            icon = fontawesome::fa_i("radiation"), 
+            color = ifelse(current_dat$radonShortTermAvg > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "Radon (pCi/L)")], "yellow",
+                           ifelse(current_dat$radonShortTermAvg < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "Radon (pCi/L)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$radonShortTermAvg > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "Radon (pCi/L)")], "this is higher than usual",
+                              ifelse(current_dat$radonShortTermAvg < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "Radon (pCi/L)")],
+                                     "this is lower than usual", ""))
+        )
+    })
+    output$co2_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$co2, " ppm"),
+            title = "CO2",
+            icon = icon("cloud"), 
+            color = ifelse(current_dat$co2 > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "CO2 (ppm)")], "yellow",
+                           ifelse(current_dat$co2 < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "CO2 (ppm)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$co2 > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "CO2 (ppm)")], "this is higher than usual",
+                              ifelse(current_dat$co2 < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "CO2 (ppm)")],
+                                     "this is lower than usual", ""))
+        )
+    })
+    output$voc_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$voc, " ppb"),
+            title = "VOCs",
+            icon = fontawesome::fa_i("wind"), 
+            color = ifelse(current_dat$voc > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "VOC (ppb)")], "yellow",
+                           ifelse(current_dat$voc < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "VOC (ppb)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$voc > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "VOC (ppb)")], "this is higher than usual",
+                              ifelse(current_dat$voc < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "VOC (ppb)")],
+                                     "this is lower than usual", ""))
+        )
+    })
+    output$pm2.5_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$pm25, " mg/m3"),
+            title = "PM 2.5",
+            icon = fontawesome::fa_i("smog"), 
+            color = ifelse(current_dat$pm25 > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "PM2.5 (mg/m3)")], "yellow",
+                           ifelse(current_dat$pm25 < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "PM2.5 (mg/m3)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$pm25 > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "PM2.5 (mg/m3)")], "this is higher than usual",
+                              ifelse(current_dat$pm25 < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "PM2.5 (mg/m3)")],
+                                     "this is lower than usual", ""))
+        )
+    })
+    output$pm10_current <- renderInfoBox({
+        infoBox(
+            value = paste0(current_dat$pm1, " mg/m3"),
+            title = "PM 10",
+            icon = icon("smog"), 
+            color = ifelse(current_dat$pm1 > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                       "PM10 (mg/m3)")], "yellow",
+                           ifelse(current_dat$pm1 < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                              "PM10 (mg/m3)")],
+                                  "light-blue", "olive")),
+            subtitle = ifelse(current_dat$pm1 > get_avg.med$Q3[which(get_avg.med$metric == 
+                                                                          "PM10 (mg/m3)")], "this is higher than usual",
+                              ifelse(current_dat$pm1 < get_avg.med$Q1[which(get_avg.med$metric == 
+                                                                                 "PM10 (mg/m3)")],
+                                     "this is lower than usual", ""))
+        )
+    })
+    
+    
+    
+#     output$airplot <- renderPlotly({
+#         req(input$metric)
+#         show_dat <- dat()
+#     
+#         air_plot <- list()
+#         air_plot_names <- list()
+#         for(i in unique(show_dat$metric)){
+#             
+#             air_plot2 <- ggplot(show_dat %>% 
+#                                         filter(metric == i), 
+#                                     aes(x = recorded, y = Result, color = day(recorded))) + 
+#                 geom_line() + 
+#                 viridis::scale_color_viridis() + 
+#                 facet_wrap(~metric, scales = "free_y") + 
+#                 ggthemes::theme_pander() +
+#                 xlab("Date")+
+#                 ylab("Concentration") + 
+#                 theme(legend.position = "none",
+#                       panel.grid.major.y = element_blank(),
+#                       panel.grid.major.x = element_line(color = "snow2"),
+#                       strip.text.x = element_text(color = "#556B2F", face = "bold"),
+#                       text = element_text(family = "Arial"))
+#             
+#                 air_plot[[i]] <-  ggplotly(air_plot2, 
+#                                            tooltip = c("y", "label", "label2"),
+#                                            height = 300*length(unique(show_dat$metric))) 
+# 
+# 
+#             
+#         }
+#         
+#         
+#         
+#         subplot(c(air_plot), nrows = length(unique(show_dat$metric)))
+# 
+# })
     
     
     output$airplot <- renderPlotly({
-        req(input$metric)
         show_dat <- dat()
         
-        # air_plot <- ggplot(show_dat) + 
-        #     geom_line(aes(x = recorded, y = Result ,
-        #                   color = day(recorded),
-        #                   label = Time,
-        #                   label2 = date)) +
-        #     viridis::scale_color_viridis() + 
-        #     facet_wrap(~metric, ncol = 1, scales = "free_y") + 
-        #     ggthemes::theme_pander() +
-        #     xlab("Date")+
-        #     ylab("Concentration") + 
-        #     theme(legend.position = "none",
-        #           panel.grid.major.y = element_blank(),
-        #           panel.grid.major.x = element_line(color = "snow2"),
-        #           strip.text.x = element_text(color = "#556B2F", face = "bold"),
-        #           text = element_text(family = "Arial"))
-        # 
-        # #print figure
-        # ggplotly(air_plot, 
-        #          tooltip = c("y", "label", "label2"),
-        #          height = length(unique(show_dat$metric))*300) 
-        
-        
-        air_plot <- list()
-        air_plot_names <- list()
-        for(i in unique(show_dat$metric)){
-            
-            air_plot2 <- ggplot(show_dat %>% 
-                                        filter(metric == i), 
-                                    aes(x = recorded, y = Result, color = day(recorded))) + 
-                geom_line() + 
-                viridis::scale_color_viridis() + 
-                facet_wrap(~metric, scales = "free_y") + 
-                ggthemes::theme_pander() +
-                xlab("Date")+
-                ylab("Concentration") + 
-                theme(legend.position = "none",
-                      panel.grid.major.y = element_blank(),
-                      panel.grid.major.x = element_line(color = "snow2"),
-                      strip.text.x = element_text(color = "#556B2F", face = "bold"),
-                      text = element_text(family = "Arial"))
-            
-                air_plot[[i]] <-  ggplotly(air_plot2, 
-                                           tooltip = c("y", "label", "label2"),
-                                           height = 300*length(unique(show_dat$metric))) 
+        air_plot <- ggplot(dat()) +
+            geom_line(aes(x = recorded, y = Result ,
+                          color = day(recorded),
+                          label = Time,
+                          label2 = date)) +
+            viridis::scale_color_viridis() +
+            facet_wrap(~metric, ncol = 1, scales = "free_y") +
+            ggthemes::theme_pander() +
+            xlab("Date")+
+            ylab("Concentration") +
+            theme(legend.position = "none",
+                  panel.grid.major.y = element_blank(),
+                  panel.grid.major.x = element_line(color = "snow2"),
+                  strip.text.x = element_text(color = "#556B2F", face = "bold"),
+                  text = element_text(family = "Arial"))
 
-
-            
-        }
-        
-        
-        
-        subplot(c(air_plot), nrows = length(unique(show_dat$metric)))
-
-})
+        #print figure
+        ggplotly(air_plot,
+                 tooltip = c("y", "label", "label2"),
+                 height = length(unique(show_dat$metric))*300)
+    })
 
 })
--- airthings_explorer_app/ui.R
+++ airthings_explorer_app/ui.R
@@ -7,30 +7,92 @@
 #    http://shiny.rstudio.com/
 #
 
-library(shiny)
-library(shinythemes)
-library(shinyWidgets)
-library(plotly)
-library(tidyverse)
 
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
+dashboardPage(
+    
     # Application title
-    titlePanel("EH 252 - Airthings Explorer"),
-
+    dashboardHeader(title = "EH 252  Airthings Explorer"),
+    
     # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            uiOutput("date_range"),
-            uiOutput("metric"),
-            
-        ),
+    dashboardSidebar(
+        pickerInput("date_select", "Select Date Range to Show", choices = c("Date Range" = "dates", 
+                                                                    "12 hours" = "12hr", 
+                                                                    "24 hours" = "24hr",
+                                                                    "36 hours" = "36hr",
+                                                                    "48 hours" = "48hr", 
+                                                                    "1 week" = "1wk", 
+                                                                    "1 month" = "1mo"),
+                    options = list(`actions-box` = TRUE,
+                                   title = "Select"),
+                    multiple = FALSE),
+        conditionalPanel(condition = "input.date_select ==`dates`", 
+                         dateRangeInput("date_range", "Date Range:", 
+                                        start = min(air_dat_long$date),
+                                        end = max(air_dat_long$date),
+                                        min = min(air_dat_long$date),
+                                        max = max(air_dat_long$date))),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotlyOutput("airplot")
-        )
+        #uiOutput("metric"),
+        sidebarMenu(menuItem("Dashboard", tabName = "dashboard")),
+        sidebarMenu(menuItem("Metrics", tabname = "metrics", 
+                             menuSubItem("Temperature",
+                                         tabName = "temp"),
+                             menuSubItem("Humidity",
+                                         tabName = "humidity"),
+                             menuSubItem("Pressure",
+                                         tabName = "pressure"),
+                             menuSubItem("Radon",
+                                         tabName = "radon"),
+                             menuSubItem("CO2",
+                                         tabName = "co2"),
+                             menuSubItem("VOC",
+                                         tabName = "voc"),
+                             menuSubItem("PM",
+                                         tabName = "pm"))),
+        sidebarMenu(menuItem("Source code", 
+                             icon = icon("github"), 
+                             href = "https://github.com/amandabhernandez/airthings_explorer"))
+        
+    ),
+    
+    # Show a plot of the generated distribution
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "dashboard",
+                    fluidRow(h3(" Current Measurements"),
+                             infoBoxOutput("temp_current"),
+                             infoBoxOutput("humidity_current"),
+                             infoBoxOutput("pressure_current"),
+                             infoBoxOutput("radon_current"),
+                             infoBoxOutput("co2_current"),
+                             infoBoxOutput("voc_current"),
+                             infoBoxOutput("pm2.5_current"),
+                             infoBoxOutput("pm10_current"),
+                             ),
+                    fluidRow(h3(" Time Series Graphs"), 
+                             plotlyOutput("airplot"))
+                    # fluidRow(box(
+                    #     # width = 8, status = "info", solidHeader = TRUE,
+                    #     # title = "Popularity by package (last 5 min)",
+                    #     plotlyOutput("airplot"))
+                    # )
+            ),
+            tabItem(tabName = "temp",
+                    fluidRow(plotlyOutput("tempplot"))),
+            tabItem(tabName = "humidity",
+                    fluidRow(plotlyOutput("humplot"))),
+            tabItem(tabName = "pressure",
+                    fluidRow(plotlyOutput("pressplot"))),
+            tabItem(tabName = "radon",
+                    fluidRow(plotlyOutput("radplot"))),
+            tabItem(tabName = "co2",
+                    fluidRow(plotlyOutput("co2plot"))),
+            tabItem(tabName = "voc",
+                    fluidRow(plotlyOutput("vocplot"))),
+            tabItem(tabName = "pm",
+                    fluidRow(plotlyOutput("pmplot")))
+            )
     )
-))
+    
+)
+
